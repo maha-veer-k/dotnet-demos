c75b31f44a56d09d11da64fce28ac5d8e3ee96bc
maha-veer-k <mahaveer.s@codestoresolutions.com>
2022-01-21 19:09:23 +0530

    Implement generic class

---
+++ FirstAppDemo/GenericClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAppDemo
+{
+    public class Calculator
+    {
+        public T Addition<T>(T key1, T key2)
+        {
+            dynamic dx = key1, dy = key2;
+            return dx + dy;
+        }
+    }
+    public class GenericClass
+    {
+        static void main(String[] args)
+        {
+            Calculator math = new Calculator();
+            int result =  math.Addition<int>(2, 4);
+            Console.WriteLine(result);
+        }
+        
+    }
+}
